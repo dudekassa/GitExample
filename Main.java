0bb9314b6afc9cb35687da9916e85eb508c758f6
Wiktoria64 <michalowiczkurowskawiktoria@gmail.com>
2021-03-02 07:45:42 +0000

    Patrycja Wałęsa Wiktoria Michałowicz-Kurowska

--- Main.java
+++ Main.java
@@ -3,7 +3,13 @@ class Main {
     System.out.println("Hello world!");
 
     System.out.println("Przemysław Dudek");
-
+    System.out.println("michał abski");
+    System.out.println("Patrycja Wałęsa");    
+    System.out.println("Kornela Sobonkiewicz");
+    System.out.println("Wiktoria Michałowicz-Kurowska");
+    System.out.println("Julia Brzostek");
+    System.out.println("Marysia Kwaśnica");
     
   }
+  
 }
