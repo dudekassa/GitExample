df4c6283767bbe0364eee441f1eda70613bd3fdd
Przemysław Dudek <dudek.assa@gmail.com>
2021-03-02 07:49:11 +0000

    feat: 02 03 2021

--- Main.java
+++ Main.java
@@ -3,13 +3,13 @@ class Main {
     System.out.println("Hello world!");
 
     System.out.println("Przemysław Dudek");
-    System.out.println("michał abski");
+    System.out.println("Michał Żabski");
     System.out.println("Patrycja Wałęsa");    
     System.out.println("Kornela Sobonkiewicz");
     System.out.println("Wiktoria Michałowicz-Kurowska");
     System.out.println("Julia Brzostek");
     System.out.println("Marysia Kwaśnica");
-    
+    System.out.println("Mikołaj Jeznach");
   }
   
 }
